03757810850a53c88ba2dd7d8b65773cce6e5525
MichaelEhnot <mehnot@gwu.edu>
2022-02-11 11:58:10 -0500

    low pass filter

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from skimage import io
 from matplotlib import pyplot as plt
+from scipy.ndimage import gaussian_filter
 
 # name of the input file
-imname = 'bear.jpg'
+imname_low = 'bear.jpg'
 
 # read in the image
-im = io.imread(imname)
+im_low = io.imread(imname_low)
 
-io.imshow(im)
+# apply low pass filter to one image
+low_filtered = gaussian_filter(im_low, sigma=5)
+
+io.imshow(low_filtered)
 plt.show()
