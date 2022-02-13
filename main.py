9ee76d0c69f7c0322b5e941ac4b0b6e2cecd627d
MichaelEhnot <mehnot@gwu.edu>
2022-02-13 14:11:43 -0500

    pre image export

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from scipy.ndimage import gaussian_filter
 import numpy as np
 
 # name of the input file
-imname_low = 'basketball.jpg'
-imname_high = 'orange.jpg'
+imname_low = 'watermelon.jpg'
+imname_high = 'pizza.jpg'
 
 # read in the image
 im_low = io.imread(imname_low)
 im_high = io.imread(imname_high)
 
 # apply low pass filter to one image
-s = 20
+s = 13
 r_blur = gaussian_filter(im_low[:,:,0], sigma=s)
 g_blur = gaussian_filter(im_low[:,:,1], sigma=s)
 b_blur = gaussian_filter(im_low[:,:,2], sigma=s)
 low_filtered = np.dstack([r_blur, g_blur, b_blur])
 
 # apply high pass filter to other image
-s = 7
+s = 4
 r_blur = gaussian_filter(im_high[:,:,0], sigma=s)
 g_blur = gaussian_filter(im_high[:,:,1], sigma=s)
 b_blur = gaussian_filter(im_high[:,:,2], sigma=s)
@@ -44,4 +44,8 @@ plt.show()
 io.imshow(hybrid)
 plt.show()
 
-# notes: (15,5)for sample, (8,5) for quarter, (20,5) for fruit, (20,7) for basketball
+#show log magnitude of forier transform
+plt.imshow(np.log(np.abs(np.fft.fftshift(np.fft.fft2(hybrid)))))
+plt.show()
+
+# notes: (15,5) for sample, (8,5) for quarter, (20,5) for fruit, (20,7) for basketball, (13,4) for pizza, (2) for final blur
